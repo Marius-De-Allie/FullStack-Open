dd957c683eb08de34e547587a7d01380503ff454
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-03 09:30:04 -0400

    Exercise 1.7 unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 // Header comp.
@@ -8,13 +8,14 @@ const Header = ({text}) => <h1>{text}</h1>;
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>;
 
 // StatItem comp.
-const StatItem = ({item, total}) => <p>{item} {total}</p>;
+const StatItem = ({item, value}) => <p>{item} {value}</p>;
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
 
   // Handle click of good button.
   const onGoodClick = () => setGood(good + 1);
@@ -25,6 +26,8 @@ const App = () => {
   // Handle click of bad button.
   const onBadClick = () => setBad(bad + 1);
 
+  // Total feedback value.
+  let total = good + neutral + bad;
   return (
     <div>
       <Header text="Give Feedback" />
@@ -32,9 +35,12 @@ const App = () => {
       <Button handleClick={onNeutralClick} text="neutral" />
       <Button handleClick={onBadClick} text="bad" />
       <h2>Statistics</h2>
-      <StatItem item="good" total={good} />
-      <StatItem item="neutral" total={neutral} />
-      <StatItem item="bad" total={bad} />
+      <StatItem item="good" value={good} />
+      <StatItem item="neutral" value={neutral} />
+      <StatItem item="bad" value={bad} />
+      <StatItem item="all" value={total} />
+      <StatItem item="average" value={(total) / 3} />
+      <StatItem item="positive" value={`${((good/total) * 100)} %`} />
     </div>
   )
 }
