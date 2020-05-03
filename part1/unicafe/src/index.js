ae582835391756347d189a015b956734e8e48460
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-03 17:42:30 -0400

    Exercise 1.11 unicafe

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 // Header comp.
@@ -8,14 +8,15 @@ const Header = ({text}) => <h1>{text}</h1>;
 const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>;
 
 // StatItem comp.
-const StatItem = ({name, value}) => <p>{name} {value}</p>;
+const StatItem = ({name, value}) => (
+  <tr>
+    <td>{name}</td> 
+    <td>{value}</td>
+  </tr>
+);
 
 // Statistics comp.
-const Statistic = ({name, value}) => {
-  return (
-      <StatItem name={name} value={value} />
-  )
-};
+const Statistic = ({name, value}) => <StatItem name={name} value={value} />;
 
 const App = () => {
   // save clicks of each button to own state
@@ -43,14 +44,16 @@ const App = () => {
       <Button handleClick={onBadClick} text="bad" />
       <h2>Statistics</h2>
       {good === 0 && neutral === 0 && bad === 0 ? <p>No feedback given</p> : 
-        <Fragment>
-          <Statistic name="good" value={good} />
-          <Statistic name="bad" value={bad} />
-          <Statistic name="neutral" value={neutral} />
-          <Statistic name="all" value={total} />
-          <Statistic name="average" value={total / 3} />
-          <Statistic name="positive" value={(good / total) * 100} />
-        </Fragment>
+        <table>
+          <tbody>
+            <Statistic name="good" value={good} />
+            <Statistic name="bad" value={bad} />
+            <Statistic name="neutral" value={neutral} />
+            <Statistic name="all" value={total} />
+            <Statistic name="average" value={total / 3} />
+            <Statistic name="positive" value={(good / total) * 100} />
+          </tbody>
+        </table>
       }
     </div>
   )
