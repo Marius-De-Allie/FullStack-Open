210f8a0c34665cfe287a93cf586ef7510609a3ee
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-03 01:33:46 -0400

    Exercise 1.2

--- part1/course-info/src/index.js
+++ part1/course-info/src/index.js
@@ -7,13 +7,29 @@ const Header = ({course}) => (
   </Fragment>
 );
 
+const Part = (props) => (
+  <Fragment>
+    <p>{`${props.part} ${props.exercises}`}</p>
+  </Fragment>
+);
+
+
 const Content = (props) => {
   const {part1, part2, part3, exercises1, exercises2, exercises3} = props;
   return (
     <Fragment>
-      <p>{`${part1} ${exercises1}`}</p>
-      <p>{`${part2} ${exercises2}`}</p>
-      <p>{`${part3} ${exercises3}`}</p>
+      <Part 
+        part={part1} 
+        exercises={exercises1}
+      />
+      <Part 
+        part={part2} 
+        exercises={exercises2} 
+      />
+      <Part 
+        part={part3} 
+        exercises={exercises3}
+      />
     </Fragment>
   )
 };
