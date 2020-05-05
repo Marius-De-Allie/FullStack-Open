457d0248e45ffd8f414436eb39dd6bf36048b365
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-05 19:56:16 -0400

    Exercise 2.11 The phonebook

--- part2/the-phone-book/src/App.js
+++ part2/the-phone-book/src/App.js
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import SearchFilter from './components/SearchFilter';
 import Persons from './components/Persons';
 import AddPerson from './components/AddPerson';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
   // COMPONENT STATE.
+  const [ persons, setPersons ] = useState([]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
   const [ searchTerm, setSearchTerm ] = useState('');
   const [ filteredPersons, setFilteredPersons ] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log(response);
+      setPersons(response.data);
+    });
+  }, [])
 
   const nameChange = (evt) => {
     const value = evt.target.value.trimStart();
