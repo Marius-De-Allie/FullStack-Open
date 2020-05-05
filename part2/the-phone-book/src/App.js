3a8a28ea3e76d89ccd14a85cc360570c0c88524f
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-05 16:44:55 -0400

    Exercise 2.8 The phonebook

--- part2/the-phone-book/src/App.js
+++ part2/the-phone-book/src/App.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '905-467-9546' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('');
+  const [ newNumber, setNewNumber ] = useState('');
 
-  const inputChange = (evt) => {
+  const nameChange = (evt) => {
     const value = evt.target.value.trimStart();
     setNewName(value);
   };
+
+  const numberChange = (evt) => {
+    const number = evt.target.value.trimStart();
+    setNewNumber(number);
+  }
 
   const formSubmit = (evt) => {
     evt.preventDefault();
     // Create a new persons array with every user name in uppercase characters.
     const personsArray = persons.map(person => person.name.toUpperCase());
     const newNameUpper = newName.toUpperCase();
-    console.log('newName', newNameUpper)
-    console.log('Array', personsArray)
-
-    console.log(personsArray.includes(newNameUpper))
     // Create newPerson object to be added to persons array piece of component state.
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
     // Check whether persons array already contains the name of user attempting to be added.
     if(personsArray.includes(newNameUpper)) {
@@ -30,8 +33,9 @@ const App = () => {
     } else {
       setPersons(persons.concat(newPerson));
     }
-    // Reset input field value to an empty string.
+    // Reset both input fields value to an empty string.
     setNewName('');
+    setNewNumber('');
   }
 
   return (
@@ -40,8 +44,18 @@ const App = () => {
       <form onSubmit={formSubmit}>
         <div>
           name: <input 
+            type="text"
+            placeholder="Please enter name"
             value={newName} 
-            onChange={inputChange}
+            onChange={nameChange}
+          />
+        </div>
+        <div>
+          number: <input 
+            type="tel"
+            placeholder="Please enter phone #"
+            value={newNumber} 
+            onChange={numberChange}
           />
         </div>
         <div>
@@ -49,7 +63,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
