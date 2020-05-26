f8f60c9af0bcc7583570c579d206ef3a1bd78656
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-26 00:56:16 -0400

    Exercise 6.21 anecdotes

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -4,21 +4,35 @@ const REMOVE_NOTIFICATION = 'REMOVE_NOTIFICATION';
 
 // Action creeators.
 
-const setNotification = message => ({
+const setNotification = (message) => ({
     type: SET_NOTIFICATION,
     message
+    
 });
 
 const removeNotification = () => ({
     type: REMOVE_NOTIFICATION
 });
 
+// let nextNotificatinId = 0;
 const handleNotification = (message, time) => {
-    return async dispatch => {
-        dispatch(setNotification(message))
-        setTimeout(() => {
-            dispatch(removeNotification())
-        }, time)
+    return (dispatch , getState) => {
+        
+        console.log('NOTIF', getState())
+        let timerId;
+        if(getState().notification.length === 0) {
+            dispatch(setNotification(message));
+            timerId = setTimeout(() => {
+                dispatch(removeNotification())
+            }, time)
+            clearTimeout(timerId)
+        } else if(getState().notification.length > 0) {
+            window.clearTimeout(timerId)
+            dispatch(setNotification(message));
+            setTimeout(() => {
+                dispatch(removeNotification())
+            }, time)
+        }
     }
 };
 
