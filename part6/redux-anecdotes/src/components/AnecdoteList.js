7ebaece008f357c355427aac25d2803c514ce182
Marius-De-Allie <spectre4300s@yahoo.com>
2020-05-25 21:54:43 -0400

    Exercise 6.19 anecdotes and connect

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,39 +1,18 @@
 import React, { Fragment } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import{ handleAddVote } from '../reducers/anecdoteReducer';
 import { handleNotification } from '../reducers/notificationReducer';
 
-const AnecdoteList = () => {
-    const dispatch = useDispatch();
-
-    // const notes = useSelector(state => {
-    //     if ( state.filter === 'ALL' ) {
-    //       return state.notes
-    //     }
-    //     return state.filter  === 'IMPORTANT' 
-    //       ? state.notes.filter(note => note.important)
-    //       : state.notes.filter(note => !note.important)
-    //   })
-    
-    const anecdotes = useSelector(state => {
-        let anects;
-        if(state.filter === '') {
-            anects =  state.anecdotes;
-        } else {
-            anects = state.anecdotes.filter(anec => anec.content.includes(state.filter))
-        }
-        return anects.sort((a, b) => b.votes - a.votes)
-    });
-
+const AnecdoteList = ({anecdotes, handleAddVote, handleNotification}) => {
     const handleVote = (id, anecdote) => {
         const updatedAnec = {
             ...anecdote,
             votes: anecdote.votes + 1
         };
         // dispatch handle add vote thunk action.
-        dispatch(handleAddVote(id, updatedAnec))
+        handleAddVote(id, updatedAnec);
         // dispatch SET_NOTIFICATION action.
-        dispatch(handleNotification(`you voted '${anecdote.content}'`, 5000));
+        handleNotification(`you voted '${anecdote.content}'`, 5000);
     };
 
     return (
@@ -53,4 +32,21 @@ const AnecdoteList = () => {
     );
 };
 
-export default AnecdoteList;
+const mapDispatchToProps = ({
+    handleAddVote,
+    handleNotification
+});
+
+const mapStateToprops = (state) => {
+    let anecdotes;
+    if(state.filter === '') {
+        anecdotes =  state.anecdotes;
+    } else {
+        anecdotes = state.anecdotes.filter(anec => anec.content.includes(state.filter));
+    }
+    return {
+        anecdotes: anecdotes.sort((a, b) => b.votes - a.votes)
+    }
+
+}
+export default connect(mapStateToprops, mapDispatchToProps)(AnecdoteList);
