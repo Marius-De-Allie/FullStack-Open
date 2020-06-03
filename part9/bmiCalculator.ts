59cb36b12c2278d48814c520aaf27a5265549719
Marius-De-Allie <spectre4300s@yahoo.com>
2020-06-03 15:31:02 -0400

    Exercise 9.3 Command line

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,17 +1,43 @@
-const calculateBmi = (height: number, weigth: number): string =>  {
-    const bmi = weigth / (height * height) * 10000;
+interface BmiValues {
+    value1: number;
+    value2: number;
+};
+
+const parseArguments = (args: Array<string>): BmiValues => {
+    if(args.length < 4) throw new Error('Not enough arguments');
+    if(args.length > 4) throw new Error('Too many arguments');
+
+    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
+const calculateBmi = (height: number, weight: number): string =>  {
+    const bmi = weight / (height * height) * 10000;
     let message;
     if(bmi < 18.5) {
         message = 'Under weight (un-healthy weight)';
-    } else if(bmi >= 18.5) {
+    } else if(bmi >= 18.5 && bmi <= 25) {
         message = 'Normal (healthy weight)';
-    } else if(bmi > 25) {
+    } else if(bmi > 25 && bmi <= 30) {
         message = 'Overweight (un-healthy weight)';
     } else if(bmi > 30){
         message = 'Obese (un-healthy weight)';
     }
-
     return message;
 };
 
-console.log(calculateBmi(180, 74))
+// const height: number = Number(process.argv[2])
+// const weight: number = Number(process.argv[3])
+try {
+    const {value1, value2} = parseArguments(process.argv);
+    console.log(calculateBmi(value1, value2));
+} catch(e) {
+    console.log('Error, something bad happened, message:', e.message)
+};
+
--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -1,3 +1,38 @@
+interface ExerciseValues {
+    value1: number;
+    value2: Array<number>;
+};
+
+const parseArgs = (args: Array<string>): ExerciseValues => {
+    let val1;
+    let val2: Array<number> = [];
+    if(args.length < 4) throw new Error('Not enough arguments');
+
+    const valueArgs = args.slice(2);
+
+    // console.log(valueArgs[0])
+
+    // valueArgs.forEach(el => console.log(!isNaN(Number(el) )));
+
+    
+
+    valueArgs.forEach(el => {
+        if(!isNaN(Number(el))) {
+            val1 = Number(valueArgs[0]);
+            val2.push(Number(el))
+        }
+        else {
+            throw new Error('Provided values were not numbers!')
+        }
+    });
+    return {
+        value1: val1,
+        value2: val2
+    }
+}
+
+
+
 interface Result {
     periodLength: number,
     trainingDays: number,
@@ -7,7 +42,7 @@ interface Result {
     target: number,
     average: number
 }
-const calculateExercises = (hours: Array<number>, target: number): Result => {
+const calculateExercises = (target: number, hours: Array<number>): Result => {
 
     const trainingDaysArray = hours.filter(el => el > 0);
     let avg: number;
@@ -53,4 +88,12 @@ const calculateExercises = (hours: Array<number>, target: number): Result => {
     }
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+    const {value1, value2} = parseArgs(process.argv);
+    console.log(calculateExercises(value1, value2));
+} catch(e) {
+    console.log('Error, something bad happened, message:', e.message)
+};
+
+
+// console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
